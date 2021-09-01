b30e29ed014fa3cad40bc2377d5390e931a47ff7
mekert <mikeekert@gmail.com>
2021-09-01 15:56:05 -0500

    initial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'methodRaidTools';
+  input: FormControl;
+
+  constructor() {
+    this.input = new FormControl()
+  }
+
+  private static parseNoteCode(val: string) {
+    const i = val.split(' ');
+    i.map( (block, idx) => {
+      if (block.startsWith('|cff')) {
+        const playerName = this.findPlayerName(block);
+        if (playerName.toLowerCase() !== 'everyone') {
+          const v = `{p:${playerName}\}${block} ${i[idx + 1].trim()}{/p}`
+          i.splice(idx + 1, 1)
+          i[idx] = v;
+        }
+      }
+      return block;
+    })
+    return i.join(' ');
+  }
+
+  private static findPlayerName(str: string) {
+    return str.substring(10).slice(0, -2)
+  }
+
+  convertClick() {
+    this.input.patchValue(AppComponent.parseNoteCode(this.input.value))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,9 +8,10 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
